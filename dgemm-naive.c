56459488880768b23e69e4877b8b139a478d860f
Ziyang Wang <ziyang@Ziyangs-MacBook-Pro.local>
2018-01-31 23:54:39 -0800

    commit from bash script

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -13,7 +13,7 @@ void square_dgemm (int n, double* A, double* B, double* C)
 {
 #ifdef TRANSPOSE
   for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j) {
+    for (int j = i; j < n; ++j) {
         double t = B[i*n+j];
         B[i*n+j] = B[j*n+i];
         B[j*n+i] = t;
